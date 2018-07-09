12989c88d2e0f3ace80140e05f28ef5ca7750e3f
LiBai666 <overwall2016@gmail.com>
2018-07-09 11:55:24 +0800

    refactor: fix Card style and add visit button

--- src/components/Note/Card/Card.vue
+++ src/components/Note/Card/Card.vue
@@ -1,12 +1,16 @@
 <template>
 <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-        <div class="card-header">
+        <div class="card-header smaller-header">
           {{note.domain}} 
-          <button @click="editable" class="btn btn-info">{{contentEditable ? "finish" : "edit"}}</button>
+          <button @click="editable" class="btn btn-info smaller-btn">{{contentEditable ? "finish" : "edit"}}</button>
+          <a :href="url">
+              <button class="btn btn-primary smaller-btn">visit</button>
+            </a>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">{{note.path}}</h5>
-          <p class="card-text" :contenteditable="contentEditable">{{content}}</p>
+        <div class="card-body smaller-body">
+          <h5 class="card-title smaller-title">{{note.path}}</h5>
+          <p :class="{'edit': contentEditable,'card-text': true}" 
+             :contenteditable="contentEditable">{{content}}</p>
         </div>
       </div>
 </template>
@@ -21,6 +25,11 @@
         content: this.note.content
       }
     },
+    computed:{
+      url: function(){
+        return this.note.domain + '/' + this.note.path
+      }
+    },
     methods: {
       editable () {
         this.contentEditable = !this.contentEditable
@@ -30,3 +39,21 @@
 
 	}
 </script>
+<style type="text/css">
+  .edit {
+    background-color: white;
+    color: black;
+  }
+  .smaller-btn {
+    padding: 0.1rem 0.7rem;
+  }
+  .smaller-header {
+    padding: 0.4rem 0.4rem;
+  }
+  .smaller-body {
+    padding: 0.6rem;
+  }
+  .smaller-title {
+    margin-bottom: 0.3rem;
+  }
+</style>
